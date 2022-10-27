de8de1871e0d0fe1bb4e74b160928ce7c9f5c4e4
cmavelis <cmavelis@gmail.com>
2022-10-26 22:40:51 -0700

    top and bottom paddles

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -5,11 +5,28 @@ export default class Demo extends Phaser.Scene {
   // private paddle: ;
   // private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
   private ball: any;
-  private paddle: any;
+  private paddles: any;
+  private paddleObject = {};
   private cursors: any;
   constructor() {
     super('GameScene');
-    // this.paddle = this.physics.add.image(40, 40, 'paddle')
+  }
+
+  accelerateForward(object: any, acceleration=100, x=false, y=false) {
+    if (x) {
+      if (object.body.velocity.x >= 0) {
+        object.setAccelerationX(acceleration);
+      } else {
+        object.setAccelerationX(-acceleration);
+      }
+    }
+    if (y) {
+      if (object.body.velocity.y >= 0) {
+        object.setAccelerationY(acceleration);
+      } else {
+        object.setAccelerationY(-acceleration);
+      }
+    }
   }
 
   preload() {
@@ -18,19 +35,24 @@ export default class Demo extends Phaser.Scene {
   create() {
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    this.paddle = this.physics.add.image(40, 40, 'paddle')
-    this.paddle.setTintFill(0xffffff).setDisplaySize(160, 20)
-    this.paddle.setCollideWorldBounds(true);
-    this.paddle.setPushable(false)
-    this.paddle.setBounce(0.5)
+    this.paddles = this.physics.add.group()
 
+    this.paddleObject.top = this.paddles.create(40, 40, 'paddle', )
+    this.paddleObject.bottom = this.paddles.create(40, 560, 'paddle')
+    this.paddles.children.iterate((c: any) => {
+      c.setTintFill(0xffffff)
+      .setDisplaySize(160, 20)
+      .setCollideWorldBounds(true)
+      .setPushable(false)
+      .setBounce(0.5)
+    })
 
     this.ball = this.physics.add.image(20, 20, 'ball')
     this.ball.setTintFill(0xffffff)
     this.ball.setCollideWorldBounds(true);
     this.ball.setBounce(1)
 
-    this.physics.add.collider(this.paddle, this.ball)
+    this.physics.add.collider(this.paddles, this.ball)
 
     let ballSpeed
     this.ball.setVelocityX(140)
@@ -38,21 +60,19 @@ export default class Demo extends Phaser.Scene {
   }
 
   update() {
-
     const paddleAccel = 1000;
-    if (this.cursors.left.isDown) {
-      this.paddle.setAccelerationX(-paddleAccel);
-    } else if (this.cursors.right.isDown) {
-      this.paddle.setAccelerationX(paddleAccel);
-    } else if (this.cursors.up.isDown) {
-      if (this.paddle.body.velocity.x >= 0) {
-        this.paddle.setAccelerationX(paddleAccel);
-      } else {
-        this.paddle.setAccelerationX(-paddleAccel);
-      }
-    }else {
-      this.paddle.setAccelerationX(0);
+
+   if (this.cursors.up.isDown) {
+     this.accelerateForward(this.paddleObject.top,paddleAccel, true)
+    } else {
+     this.paddleObject.top.setAccelerationX(0);
     }
+    if (this.cursors.down.isDown) {
+      this.accelerateForward(this.paddleObject.bottom,paddleAccel, true)
+    } else {
+      this.paddleObject.bottom.setAccelerationX(0);
+    }
+
     // if (this.cursors.up.isDown)
     // {
     //   this.paddle.setVelocityY(-moveVelocity);
